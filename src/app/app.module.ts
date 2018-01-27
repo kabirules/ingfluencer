1435e0bc3f87c00c1be0d2d4155186581c7f54d1
kabirules <javifont@gmail.com>
2018-01-27 21:52:37 +0100

    Firebase login, update version pending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule // imports firebase/auth, only needed for auth features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    routes
   ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ AppComponent, LoginComponent, SignupComponent, EmailComponent, MembersComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [AuthGuard]
 })
 export class AppModule {}
---
+++ src/app/email/email.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth, AuthProviders, AuthMethods } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { moveIn, fallIn } from '../router.animations';
+
+@Component({
+  selector: 'app-email',
+  templateUrl: './email.component.html',
+  styleUrls: ['./email.component.css'],
+  animations: [moveIn(), fallIn()],
+  host: {'[@moveIn]': ''}
+})
+
+export class EmailComponent implements OnInit {
+
+  state: string = '';
+  error: any;
+
+  constructor(public af: AngularFireAuth,private router: Router) {
+    this.af.auth.subscribe(auth => { 
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(formData) {
+    if(formData.valid) {
+      console.log(formData.value);
+      this.af.auth.signInWithEmailAndPassword({
+        email: formData.value.email,
+        password: formData.value.password
+      },
+      {
+        provider: AuthProviders.Password,
+        method: AuthMethods.Password,
+      }).then(
+        (success) => {
+        console.log(success);
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        console.log(err);
+        this.error = err;
+      })
+    }
+  }
+}
